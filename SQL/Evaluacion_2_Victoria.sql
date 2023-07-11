6d5ee98670a2367cbf1d450d51b89bb8621c7806
lessa13 <victoria.s.mas@gmail.com>
2023-07-12 00:36:23 +0200

    version final

--- SQL/Evaluacion_2_Victoria.sql
+++ SQL/Evaluacion_2_Victoria.sql
@@ -5,7 +5,7 @@ con códigos: 1, 3, 7, 8 y 9, que tengan stock en el almacén, y al mismo
 tiempo que sus precios unitarios estén entre 50 y 100. 
 Por último, ordena los resultados por código de proveedor de forma ascendente.*/
 
-SELECT products.supplier_id, products.product_name
+SELECT *
 FROM products
 INNER JOIN order_details
 ON products.product_id = order_details.product_id
@@ -19,9 +19,9 @@ ORDER BY products.supplier_id;
  de cualquier año.*/
  
 
-SELECT order_id, shipped_date,employees.employee_id, employees.first_name, employees.last_name, month(shipped_date), day(shipped_date), customers.company_name
+SELECT employees.employee_id, employees.first_name, employees.last_name,customers.company_name, month(shipped_date), day(shipped_date)
 FROM orders
-INNER JOIN customers
+INNER JOIN customers /* Dado que los datos se unen a traves de la tabla orders vamos a hacer dos Inner Join vinculados a ella para abarcar todos los datos que buscamos*/
 ON orders.customer_id = customers.customer_id
 INNER JOIN employees
 ON orders.employee_id = employees.employee_id
@@ -32,7 +32,7 @@ WHERE month(shipped_date) = 12 AND day(shipped_date) BETWEEN 22 AND 31 AND custo
  el precio unitario, la cantidad, el descuento y el precio de venta después 
  de haber aplicado el descuento.*/
  
-SELECT order_details.order_id, order_details.product_id, products.product_name, order_details.unit_price, order_details.discount, (order_details.quantity * order_details.unit_price) * (1 - discount) AS Totalprice
+SELECT order_details.order_id, order_details.product_id, products.product_name, order_details.unit_price, order_details.quantity, order_details.discount, ((order_details.quantity * order_details.unit_price) * (1 - discount)) AS Totalprice
 FROM order_details
 INNER JOIN products
 ON order_details.product_id = products.product_id;
@@ -42,12 +42,25 @@ ON order_details.product_id = products.product_id;
 SELECT product_name, unit_price 
 FROM products
 WHERE unit_price > (SELECT AVG(unit_price)
-						FROM products);
+						FROM products); /*Se utiliza una subconsulta para simplificar el WHERE*/
 /*¿Qué productos ha vendido cada empleado y 
 cuál es la cantidad vendida de cada uno de ellos?*/
 SELECT orders.employee_id, order_details.product_id, sum(order_details.quantity)
 FROM orders
-RIGHT JOIN order_details
+INNER JOIN order_details
 ON orders.order_id = order_details.order_id
-GROUP BY orders.employee_id;
+GROUP BY orders.employee_id, order_details.product_id;
+/*Utilizamos dos group by para agrupar por la cantidad de producto y el tipo de producto*/
+
+/*Ahora vamos a resolver el mismo problema con una CTE*/
+WITH product_empleado AS
+	(
+    SELECT orders.employee_id, order_details.product_id, order_details.quantity
+    FROM orders /*Diseñamos una CTE que contenga aquellas columnas a partir de las cuales vamos a extraer nuestra información*/
+    INNER JOIN order_details
+    ON orders.order_id = order_details.order_id)
+			SELECT employee_id, product_id, sum(quantity) as cantidadP
+            FROM product_empleado 
+            GROUP by product_id, employee_id;
+
 
